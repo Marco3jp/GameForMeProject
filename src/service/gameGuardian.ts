41a5252cab52beefd20995db489413a4696fa844
marco <marco3jp+git@gmail.com>
2022-06-08 01:56:01 +0900

    ログ出すやつ書いた

--- src/service/gameGuardian.ts
+++ src/service/gameGuardian.ts
@@ -10,4 +10,33 @@ export class GameGuardian {
     public static resume() {
         PIXI.Ticker.shared.start()
     }
+
+    /**
+     * @param data
+     * @param key
+     */
+    public static log(data: object | string | number, key?: string) {
+        const loggerElm = document.querySelector("#logger")
+        if (!loggerElm) return
+
+        const newElm = document.createElement("pre")
+        if (typeof data === "string") {
+            newElm.textContent = data
+        } if (typeof data === "number"){
+            newElm.textContent = data.toString()
+        }else {
+            newElm.textContent = JSON.stringify(data)
+        }
+
+        if (key) {
+            newElm.classList.add(key)
+            const oldElm = document.querySelector(`.${key}`)
+            if (oldElm) {
+                oldElm.replaceWith(newElm)
+                return;
+            }
+        }
+
+        loggerElm.appendChild(newElm)
+    }
 }
