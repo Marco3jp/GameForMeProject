04f9ff2d47fd2262f66187e43d584954bff8c37f
marco <marco3jp+git@gmail.com>
2022-11-25 02:15:39 +0900

    barを動かすロジックをなんかいい感じに移植した

--- src/service/barMoving.ts
+++ src/service/barMoving.ts
@@ -2,25 +2,32 @@ import {BarMovingInterface} from "../component/service/barMoving";
 import {Bar} from "../component/bar";
 import {onMouseMoveEvent} from "../component/service/input";
 import {INITIAL_APP} from "./parameter/app";
-import {CANVAS_PADDING, WALL_THICKNESS} from "../component/stage/simpleStage/simpleStage";
+import {WALL_THICKNESS} from "../component/stage/simpleStage/simpleStage";
+import Matter from "matter-js";
 
 export class BarMoving implements BarMovingInterface{
     move(component: Bar, mouseMoving: onMouseMoveEvent): void {
-        if (!component.instance) return
+        if (!component.instance || !component.matterInstance) return
 
-        const barHalfWidth = component.instance.width / 2
-        const leftmostCoordinatesInStage = CANVAS_PADDING.X + WALL_THICKNESS
-        const rightmostCoordinatesInStage = INITIAL_APP.canvasWidth - CANVAS_PADDING.X - WALL_THICKNESS
+        const barHalfWidth =  component.getWidth() / 2
+        const leftmostCoordinatesInStage = WALL_THICKNESS
+        const rightmostCoordinatesInStage = INITIAL_APP.canvasWidth - WALL_THICKNESS
 
         if (mouseMoving.clientX < barHalfWidth + leftmostCoordinatesInStage){
             // |___           |
-            component.instance.x = 25 + 10
+            const x = WALL_THICKNESS
+            component.instance.x = x
+            Matter.Body.setPosition(component.matterInstance, Matter.Vector.create(x + barHalfWidth, component.matterInstance.position.y))
         } else if (mouseMoving.clientX >  rightmostCoordinatesInStage - barHalfWidth) {
             // |           ___|
-            component.instance.x = rightmostCoordinatesInStage - component.instance.width
+            const x = rightmostCoordinatesInStage - component.instance.width
+            component.instance.x = x
+            Matter.Body.setPosition(component.matterInstance, Matter.Vector.create(x + barHalfWidth, component.matterInstance.position.y))
         } else {
             // |   ___        |
-            component.instance.x = mouseMoving.clientX - barHalfWidth
+            const x = mouseMoving.clientX - barHalfWidth
+            component.instance.x = x
+            Matter.Body.setPosition(component.matterInstance, Matter.Vector.create(x + barHalfWidth, component.matterInstance.position.y))
         }
     }
 }
